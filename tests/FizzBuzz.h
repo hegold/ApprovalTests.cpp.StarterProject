b7faee62aa99e0482e52b05a776fd50dc5c030cb
Henry Golding <hegold@users.noreply.github.com>
2021-03-16 12:37:32 -0700

    FizzBuzz starter

---
+++ tests/FizzBuzz.h
@@ -0,0 +1,8 @@
+
+namespace FizzBuzz
+{
+    void printFizzBuzz(std::stringstream& output)
+    {
+        output << "Add code here";
+    }
+}
--- tests/NewTest.cpp
+++ tests/NewTest.cpp
@@ -2,6 +2,7 @@
 
 #include "ApprovalTests.hpp"
 #include "catch2/catch.hpp"
+#include "FizzBuzz.h"
 
 using namespace ApprovalTests;
 
@@ -9,23 +10,31 @@ using namespace ApprovalTests;
 // Please note that you will need a Diff Tool installed:
 //  https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Reporters.md#supported-diff-tools
 
-TEST_CASE("ApprovingText")
+TEST_CASE("FizzBuzzTest")
 {
-    Approvals::verify(
-        "Approval Tests can verify text via the golden master method ");
+    std::stringstream stream;
+    FizzBuzz::printFizzBuzz(stream);
+    Approvals::verify(stream.str());
 }
 
-TEST_CASE("ApprovingWithReporters")
-{
-    // See https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Options.md#top
-    // you can alter this to make the test fail and launch a diff tool:
-    Approvals::verify("Reporters launch only on failure, by default they are "
-                      "the DiffReporter",
-                      Options(DiffReporter()));
-}
+//TEST_CASE("ApprovingText")
+//{
+//    Approvals::verify(
+//        "Approval Tests can verify text via the golden master method ");
+//}
+//
+//TEST_CASE("ApprovingWithReporters")
+//{
+//    // See https://github.com/approvals/ApprovalTests.cpp/blob/master/doc/Options.md#top
+//    // you can alter this to make the test fail and launch a diff tool:
+//    Approvals::verify("Reporters launch only on failure, by default they are "
+//                      "the DiffReporter",
+//                      Options(DiffReporter()));
+//}
+//
+//TEST_CASE("Approval Is just an assertion library, you don't need to use it.")
+//{
+//    REQUIRE(std::string("No Approvals, only Catch") ==
+//            "No Approvals, only Catch");
+//}
 
-TEST_CASE("Approval Is just an assertion library, you don't need to use it.")
-{
-    REQUIRE(std::string("No Approvals, only Catch") ==
-            "No Approvals, only Catch");
-}
